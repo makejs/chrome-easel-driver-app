8d968d0a62a396be674e65c1fc89d469f8891eb5
Nathan Caza <mastercactapus@gmail.com>
2016-08-14 23:00:20 -0500

    minify output on build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf app
-(cd server && gopherjs build -o ../app/server.js)
+(cd server && gopherjs build -o ../app/server.js -m)
 node_modules/.bin/webpack -p --progress
 rm app/*.map
 cp -v static/* app/
