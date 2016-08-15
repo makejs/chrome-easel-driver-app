ba7e0529e7902a40fdf5c5e8d0a176a23518168f
Nathan Caza <mastercactapus@gmail.com>
2016-08-14 23:57:16 -0500

    close any old stuff before launching again

--- static/background.js
+++ static/background.js
@@ -1,8 +1,18 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
+  // disconnect/close all serial connections and tcp server sockets
+  const sockets = new Promise(resolve=>chrome.sockets.tcpServer.getSockets(resolve))
+  .then(sockets=>Promise.all(sockets.map(({socketId})=>new Promise(resolve=>chrome.sockets.tcpServer.close(socketId, resolve)))))
+  const sConns = new Promise(resolve=>chrome.serial.getConnections(resolve))
+  .then(conns=>Promise.all(conns.map(({connectionId})=>new Promise(resolve=>chrome.serial.disconnect(connectionId, resolve)))))
+
+  Promise.all([sockets, sConns])
+  .then(()=>chrome.app.window.create('window.html', {
+    'id': "",
+    singleton: true,
     'outerBounds': {
       'width': 400,
       'height': 500
     }
-  });
+  }))
+
 });
